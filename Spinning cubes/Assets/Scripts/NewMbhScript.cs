a5f64b3f803800fd212c4567bee1197ad0e06b5e
Kirim0N <nb.kirimon@gmail.com>
2025-09-21 21:45:34 +0500

    Script CopyCube + radius
    problems with initiating copy cubes

--- Spinning cubes/Assets/Scripts/NewMbhScript.cs
+++ Spinning cubes/Assets/Scripts/NewMbhScript.cs
@@ -5,10 +5,14 @@ public class NewMbhScript : MonoBehaviour
     [SerializeField] private GameObject cubeToCopy;
     [SerializeField] private int cubesNumber;
     [SerializeField] private Transform cubeToSpawnPlace;
+    [SerializeField] private float radius;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
+        // CubeToSpawnPlace.SetParent(cubeToCopy);
+        cubeToSpawnPlace.position += new Vector3(radius, 0, 0);
         for (var i = 0; i < cubesNumber; i++)
         {
             Instantiate(cubeToCopy, cubeToSpawnPlace);
