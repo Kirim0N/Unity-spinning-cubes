542087276df4eae1a399a0aa3091d74802dd0db4
Kirim0N <nb.kirimon@gmail.com>
2025-09-26 19:34:35 +0500

    Correct cube pefabs spawn

--- Spinning cubes/Assets/Scripts/NewMbhScript.cs
+++ Spinning cubes/Assets/Scripts/NewMbhScript.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
+// using System;
 
 public class NewMbhScript : MonoBehaviour
 {
-    [SerializeField] private GameObject cubeToCopy;
+    [SerializeField] private GameObject cubePrefab;
     [SerializeField] private int cubesNumber;
-    [SerializeField] private Transform cubeToSpawnPlace;
+    [SerializeField] private Transform cubeParent;
     [SerializeField] private float radius;
-    
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-        // CubeToSpawnPlace.SetParent(cubeToCopy);
-        cubeToSpawnPlace.position += new Vector3(radius, 0, 0);
         for (var i = 0; i < cubesNumber; i++)
         {
-            Instantiate(cubeToCopy, cubeToSpawnPlace);
+            var x = (float) Mathf.Cos((2 * Mathf.PI / cubesNumber) * i) * radius;
+            var z = (float) Mathf.Sin((2 * Mathf.PI / cubesNumber) * i) * radius;
+            Instantiate(cubePrefab, new Vector3(x, 0, z), Quaternion.Euler(0, 0, 0), cubeParent);
         }
     }
 
