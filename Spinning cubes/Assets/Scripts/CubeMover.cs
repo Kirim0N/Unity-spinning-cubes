b88c262bbcf6d8e972590f92c5859c652079cabb
Kirim0N <nb.kirimon@gmail.com>
2025-09-28 18:14:13 +0500

    Strange rotation angle change
    Cubes rotate but orbit angle works strange

--- Spinning cubes/Assets/Scripts/CubeMover.cs
+++ Spinning cubes/Assets/Scripts/CubeMover.cs
@@ -4,9 +4,11 @@ public class CubeMover : MonoBehaviour
 {
     [SerializeField] private Transform center;
     [Range(0, 10)] [SerializeField] private float speed;
+    [Range(0, 360)] [SerializeField] private float xAngle;
+    [Range(0, 360)] [SerializeField] private float zAngle;
     
     void FixedUpdate()
     {
-        center.Rotate(0, speed, 0);
+        center.rotation = Quaternion.Euler(xAngle, center.eulerAngles.y + speed, zAngle);
     }
 }
